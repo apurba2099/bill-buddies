809219ad01d58b42d8201e77c0fa3823fde3f4e2
Apurba Dutta <apurbadutta2099@gmail.com>
2025-07-10 13:05:05 +0530

    6. Selecting friend feature

--- src/App.jsx
+++ src/App.jsx
@@ -16,52 +16,34 @@ export default function App() {
     setShowAddFriend(false);
   }
 
+  //Remove the formsplit in the UI
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  //Handle & Update the form split and
+  function handleTheSelection(friend) {
+    // setSelectedFriend(friend);
+    setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+    setShowAddFriend(false);
+  }
   return (
     <>
       <div className="app">
         <div className="sidebar">
-          <FriendList friends={friends} />
+          <FriendList
+            friends={friends}
+            onSelection={handleTheSelection}
+            selectedFriend={selectedFriend}
+          />
+
           {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
           <Button onClick={handleShowFriend}>
             {!showAddFriend ? "Add Friend" : "Close"}
           </Button>
         </div>
-        <FormSplitBill />
+        {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
       </div>
     </>
-  );
-}
-
-function FriendList({ friends }) {
-  return (
-    <ul>
-      {friends.map((friend) => (
-        <Friend key={friend.id} friend={friend} />
-      ))}
-    </ul>
-  );
-}
-
-//MAIN FRIEND BOX
-function Friend({ friend }) {
-  return (
-    <li>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
-
-      {friend.balance < 0 && (
-        <p className="red">
-          You owe {friend.name} {Math.abs(friend.balance)}&#8377;
-        </p>
-      )}
-      {friend.balance > 0 && (
-        <p className="green">
-          {friend.name} owes you {Math.abs(friend.balance)}&#8377;
-        </p>
-      )}
-      {friend.balance === 0 && <p>You and {friend.name} are even!</p>}
-      <Button>Select</Button>
-    </li>
   );
 }
 
@@ -71,6 +53,51 @@ function Button({ children, onClick }) {
     <button className="button" onClick={onClick}>
       {children}
     </button>
+  );
+}
+
+function FriendList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          key={friend.id}
+          friend={friend}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
+
+//MAIN FRIEND BOX
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
+
+  return (
+    <li className={isSelected ? "selected" : ""}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}&#8377;
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}&#8377;
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even!</p>}
+
+      <Button onClick={() => onSelection(friend)}>
+        {isSelected ? "Close" : "Select"}
+      </Button>
+    </li>
   );
 }
 
@@ -117,10 +144,10 @@ function FormAddFriend({ onAddFriend }) {
   );
 }
 
-function FormSplitBill() {
+function FormSplitBill({ selectedFriend }) {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
+      <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💵 Bill Value</label>
       <input type="text" />
@@ -128,13 +155,13 @@ function FormSplitBill() {
       <label>💰 Your Expenses</label>
       <input type="text" />
 
-      <label>💰 X's Expenses</label>
+      <label>💰 {selectedFriend.name}'s Expenses</label>
       <input type="text" disabled />
 
       <label>💳Who is paying the bill?</label>
       <select>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <Button>Add</Button>
